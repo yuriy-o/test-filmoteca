c438c0400b9ee7f2903f9e824d1dcd47ed643909
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-01-09 12:15:28 +0200

    300 markupListMovies

--- src/js/api.js
+++ src/js/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const API_KEY = 'b77b3068ddcc2ce3ea23003328032394';
 export const BASE_URL = 'https://api.themoviedb.org/3';
 export const API_URL = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`;
-export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w300';
 export const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}`;
 
 export default class ApiServise {
